640165c4be0e5a35ef46f42f7e65805d96cd27c4
XnJaca <xnjaca@gmail.com>
2025-10-06 11:36:16 -0600

    Fix host and port for Render SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: node({ mode: 'standalone' }),
+  server: {
+    host: true,
+    port: process.env.PORT ? Number(process.env.PORT) : 3000
+  }
 });
